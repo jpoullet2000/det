1ecf5e9fba28edb0321176eb4294584fcd30d03b
jpoullet2000 <jeanbaptistepoullet@gmail.com>
2018-03-19 18:46:33 +0000

    Generated code by SwaggerHub

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.attribute_defs_item import AttributeDefsItem
+from swagger_server.models.classification_defs_item import ClassificationDefsItem
+from swagger_server.models.cluster import Cluster
+from swagger_server.models.dataset import Dataset
+from swagger_server.models.element_defs_item import ElementDefsItem
+from swagger_server.models.entity_defs_item import EntityDefsItem
+from swagger_server.models.enum_defs_item import EnumDefsItem
+from swagger_server.models.hdfs_path_item import HdfsPathItem
+from swagger_server.models.process import Process
+from swagger_server.models.typedefs_item import TypedefsItem

1aa82133623b3257c05214008a0e4af5036876e8
jpoullet2000 <jeanbaptistepoullet@gmail.com>
2018-05-15 10:13:42 +0200

    bug fixes

--- create_credentials.sh
+++ create_credentials.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ ! -f ~/.credentials.json ]
-then echo '{"ATLAS_LOGIN": "atlas_login", "ATLAS_PASSWORD": "atlas_password", "AMBARI_LOGIN": "ambari_login", "AMBARI_PASSWORD": "ambari_password", "TEST_FLAG": true}' > ~/.credentials.json
+then echo '{"ATLAS_LOGIN": "atlas_login", "ATLAS_PASSWORD": "atlas_password", "AMBARI_LOGIN": "ambari_login", "AMBARI_PASSWORD": "ambari_password", "DET_API_TOKEN": "mytoken", "TEST_FLAG": true}' > ~/.credentials.json
 fi

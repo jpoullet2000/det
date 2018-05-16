3282e86fd6538216a8101119fa47d4b7c840c9c3
jpoullet2000 <jeanbaptistepoullet@gmail.com>
2018-05-16 10:25:01 +0200

    mocking connection in tests admin controller

--- tests/controllers/test_admins_controller.py
+++ tests/controllers/test_admins_controller.py
@@ -4,9 +4,12 @@ from __future__ import absolute_import
 
 from flask import json
 from six import BytesIO
+from unittest.mock import Mock
 
 from det.models.typedefs_item import TypedefsItem  # noqa: E501
 from det import __version__
+
+from det.operators.atlas_typedefs_create import AtlasTypedefsCreateOperator
 from . import BaseTestCase
 
 
@@ -18,6 +21,8 @@ class TestAdminsController(BaseTestCase):
 
         create type definitions
         """
+        MockTypedefsCreateOperator = AtlasTypedefsCreateOperator
+        MockTypedefsCreateOperator.execute = Mock(return_value=False)
         Typedefs = TypedefsItem()
         response = self.client.open(
             '/detapi/{version}/typedefs'.format(version=__version__),
